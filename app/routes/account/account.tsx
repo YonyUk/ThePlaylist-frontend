e35326d462eedaafd288a5c2ddec0b0863138905
YonyUk <yonatanjoseguerraperez@gmail.com>
2025-12-19 01:30:12 -0500

    adds logout functionality

--- app/routes/account/account.tsx
+++ app/routes/account/account.tsx
@@ -1,32 +1,52 @@
-import { Outlet, useLocation } from "react-router";
+import { Outlet, useLocation, useNavigate } from "react-router";
 import { ROUTES } from "~/routes";
+import type { Route } from "./+types/account";
+import { MdLogout } from "react-icons/md";
+import { UserService } from "~/services/UserService";
 
-export default function Account() {
+export default function Account({ actionData }: Route.ComponentProps) {
 
     const menuItems = [
         {
-            label:'Settings',
-            url:ROUTES.SETTINGS
+            label: 'Settings',
+            url: ROUTES.SETTINGS
         }
     ]
 
     const location = useLocation();
+    const navigate = useNavigate();
+
+    const logout = async () => {
+        const service = UserService.get();
+        const response = await service.logout();
+        if (response)
+            navigate(ROUTES.LOGIN);
+        else
+            alert("ERROR WHILE LOGOUT");
+    }
 
     return (
         <>
             <nav className="pl-18 pt-2 bg-[#00000090]">
                 <div className="flex flex-row w-full justify-end gap-3">
                     {
-                        menuItems.map((item,index) => 
+                        menuItems.map((item, index) =>
                             <div
-                            key={index}
-                            className={`rounded-t-md cursor-pointer p-2 px-4 duration-300
+                                key={index}
+                                className={`rounded-t-md cursor-pointer p-2 px-4 duration-300
                              hover:bg-[#00000040] hover:border-b-1 
                              ${location.pathname === item.url && "bg-[#00000040] border-b-1"}`}>
                                 {item.label}
                             </div>
                         )
                     }
+                    <div
+                        className="flex flex-row p-1 px-4 bg-[#00000045] m-2 rounded-md gap-2 cursor-pointer"
+                        onClick={logout}
+                    >
+                        <h1>Logout</h1>
+                        <MdLogout size={20}/>
+                    </div>
                 </div>
             </nav>
             <Outlet />
